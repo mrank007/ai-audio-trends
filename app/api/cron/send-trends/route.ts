b1a416c9ef62a2446053c7d679fa706b0cc13116
mrank007 <michael@oistudios.com.au>
2026-02-19 10:39:57 +1100

    Update route.ts

--- app/api/cron/send-trends/route.ts
+++ app/api/cron/send-trends/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import { getTrends } from "../../../../lib/groq";
 import { sendTrendsEmail } from "../../../../lib/email";
